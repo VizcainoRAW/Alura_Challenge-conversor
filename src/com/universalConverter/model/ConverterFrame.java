90c574d0c56eab83e2e9cd917fb2e75098430025
Ivan Vizcaino <ivizcain4@cuc.edu.co>
2023-07-09 15:49:36 -0500

    Added essential JComponents and primitive visual.
    -Added essential JComponents and primitive visual elements to ConverterFrame.
    -Changed the default resolution of ConverterFrame from 800x600 to 480x360 for a more compact display.
    -Set the layout of ConverterFrame to null, allowing for custom component positioning.
    -Added componentWidth and componentHeight variables. which can be utilized in future development to specify the size of components dynamically.

--- src/com/universalConverter/model/ConverterFrame.java
+++ src/com/universalConverter/model/ConverterFrame.java
@@ -1,42 +1,127 @@
 package com.universalConverter.model;
 
-import java.awt.BorderLayout;
+import java.awt.*;
+
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 
 public abstract class  ConverterFrame extends JFrame{
 	
-	private final int width=800;
-	private final int height=600;
+	private final int width=480;
+	private final int height=360;
+	
+	private final int componentWidth=200;
+	private final int componentHeight=60;
+	
+	private JPanel contentPane;
 	
 	private JTextField firstTextField=new JTextField();
 	private JTextField secondTextField=new JTextField();
 	
-	private JComboBox firtsComboBox=new JComboBox();
+	private JComboBox firstComboBox=new JComboBox();
 	private JComboBox secondComboBox=new JComboBox();
 	
 	
 	
 	public ConverterFrame() {
 
-		setBounds(100, 100, this.width, this.height);
-		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(this.width, this.height);
 		setLocationRelativeTo(null);
+		setResizable(false);
 		
-		new JPanel();
+		contentPane = new JPanel();
+
+        contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
+        contentPane.setLayout(null);
+		
+		setContentPane(contentPane);
+		
+		
+		getFirstTextField().setBounds(25,60,componentWidth, componentHeight);
+		getFirtsComboBox().setBounds(240,60,componentWidth, componentHeight);
+		getSecondTextField().setBounds(25,160,componentWidth, componentHeight);
+		getSecondComboBox().setBounds(240,160,componentWidth, componentHeight);
+		
+		contentPane.add(getFirstTextField());
+		contentPane.add(getFirtsComboBox());
+		contentPane.add(getSecondTextField());
+		contentPane.add(getSecondComboBox());
+	
 		
 		JButton backToMenu = new JButton("Back");
-		getContentPane().add(backToMenu, BorderLayout.SOUTH);
+		contentPane.add(backToMenu);
 		backToMenu.addActionListener(e -> backToMenu());
-
 		
 	}
 	
+	
+	
+	public JTextField getFirstTextField() {
+		return firstTextField;
+	}
+
+
+
+	public void setFirstTextField(JTextField firstTextField) {
+		this.firstTextField = firstTextField;
+	}
+
+
+
+	public JTextField getSecondTextField() {
+		return secondTextField;
+	}
+
+
+
+	public void setSecondTextField(JTextField secondTextField) {
+		this.secondTextField = secondTextField;
+	}
+
+
+
+	public JComboBox getFirtsComboBox() {
+		return firstComboBox;
+	}
+
+
+
+	public void setFirtsComboBox(JComboBox firtsComboBox) {
+		this.firstComboBox = firtsComboBox;
+	}
+
+
+
+	public JComboBox getSecondComboBox() {
+		return secondComboBox;
+	}
+
+
+
+	public void setSecondComboBox(JComboBox secondComboBox) {
+		this.secondComboBox = secondComboBox;
+	}
+
+
+
+	public int getWidth() {
+		return width;
+	}
+
+
+
+	public int getHeight() {
+		return height;
+	}
+
+
+
 	public void backToMenu() {
 		setVisible(false);
 		com.universalConverter.main.setVisible(com.universalConverter.main.getMenu(), true);
--- src/com/universalConverter/view/TemperatureConverterFrame.java
+++ src/com/universalConverter/view/TemperatureConverterFrame.java
@@ -1,9 +1,15 @@
 package com.universalConverter.view;
 
 import com.universalConverter.model.ConverterFrame;
+import javax.swing.JFrame;
 
 
 public class TemperatureConverterFrame extends ConverterFrame {
+	public TemperatureConverterFrame() {
 	
-	
+		getSecondComboBox().setLocation(240, 180);
+		getSecondTextField().setLocation(25, 180);
+		getFirtsComboBox().setLocation(240, 80);
+		getFirstTextField().setLocation(25, 80);
+	}
 }
